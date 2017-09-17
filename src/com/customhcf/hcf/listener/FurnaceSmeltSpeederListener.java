144dc68c05409c826b2ea57c1ac00fec01b22475
explance <lentsalts@gmail.com>
2017-09-17 12:24:30 +0100

    Fixed furnace console error

--- src/com/customhcf/hcf/listener/FurnaceSmeltSpeederListener.java
+++ src/com/customhcf/hcf/listener/FurnaceSmeltSpeederListener.java
@@ -1,58 +1,61 @@
 package com.customhcf.hcf.listener;
 
-import java.util.Random;
-
+import com.customhcf.hcf.HCF;
 import org.bukkit.Bukkit;
 import org.bukkit.Material;
-import org.bukkit.block.Block;
-import org.bukkit.block.BlockState;
 import org.bukkit.block.BrewingStand;
 import org.bukkit.block.Furnace;
 import org.bukkit.event.EventHandler;
-import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
-import org.bukkit.event.block.Action;
+import org.bukkit.event.inventory.BrewEvent;
 import org.bukkit.event.inventory.FurnaceBurnEvent;
-import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.ShapedRecipe;
 import org.bukkit.scheduler.BukkitRunnable;
 
-import com.customhcf.hcf.HCF;
+public class FurnaceSmeltSpeederListener implements Listener {
 
-public class FurnaceSmeltSpeederListener
-implements Listener {
-	public FurnaceSmeltSpeederListener() {
-		final ShapedRecipe cmelon = new ShapedRecipe(new ItemStack(Material.SPECKLED_MELON, 1));
-		cmelon.shape(new String[] { "AAA", "CBA", "AAA" }).setIngredient('B', Material.MELON).setIngredient('C', Material.GOLD_NUGGET);
-		Bukkit.getServer().addRecipe(cmelon);
-		}
-    @EventHandler(ignoreCancelled=true, priority=EventPriority.MONITOR)
-    public void onPlayerInteract(PlayerInteractEvent event) {
-        Block block;
-        BlockState state;
-        if (event.getAction() == Action.RIGHT_CLICK_BLOCK && (state = (block = event.getClickedBlock()).getState()) instanceof Furnace) {
-            ((Furnace)state).setCookSpeedMultiplier(6.0);
-        }
+    public FurnaceSmeltSpeederListener(HCF plugin) {
+        final ShapedRecipe cmelon = new ShapedRecipe(new ItemStack(Material.SPECKLED_MELON, 1));
+        cmelon.shape(new String[] { "AAA", "CBA", "AAA" }).setIngredient('B', Material.MELON).setIngredient('C', Material.GOLD_NUGGET);
+        Bukkit.getServer().addRecipe(cmelon);
     }
-    private void startUpdate(final Furnace tile, final int increase) {
-		new BukkitRunnable() {
-			public void run() {
-				if ((tile.getCookTime() > 0) || (tile.getBurnTime() > 0)) {
-					tile.setCookTime((short) (tile.getCookTime() + increase));
-					tile.update();
-				} else {
-					cancel();
-				}
-			}
-		}.runTaskTimer(HCF.getPlugin(), 1L, 1L);
-	}
 
-	@EventHandler
-	public void onFurnaceBurn(FurnaceBurnEvent event) {
-		Random RND = new Random();
-		startUpdate((Furnace) event.getBlock().getState(), RND.nextBoolean() ? 1 : 2);
-	}
+    private void startUpdate2(final BrewingStand tile, final int increase) {
+        new BukkitRunnable() {
+            public void run() {
+                if (tile.getBrewingTime() > 0) {
+                    tile.setBrewingTime((short) (tile.getBrewingTime() + increase));
+                    tile.update();
+                }else{
+                    cancel();
+                }
+            }
+        }.runTaskTimer(HCF.getInstance(), 1L, 1L);
+    }
+
+    private void startUpdate(final Furnace tile, final int increase){
+        new BukkitRunnable(){
+            public void run(){
+                if ((tile.getCookTime() > 0) || (tile.getBurnTime() > 0)){
+                    tile.setCookTime((short)(tile.getCookTime() + increase));
+                    tile.update();
+                }else{
+                    cancel();
+                }
+            }
+        }.runTaskTimer(HCF.getInstance(), 1L, 1L);
+    }
+
+    @EventHandler
+    public void onFurnaceBurn(FurnaceBurnEvent event){
+        startUpdate((Furnace)event.getBlock().getState(), 3);
+    }
+
+    @EventHandler
+    public void onBrew(BrewEvent event){
+        startUpdate2((BrewingStand) event.getBlock().getState(), 5);
+    }
+
+
 }
-
-
