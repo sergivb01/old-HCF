5009301a5ac5be1cf950b2178ff5d6b0b5fc5375
unknown <sergivb01@gmail.com>
2017-09-21 16:59:37 +0200

    rollback

--- src/com/customhcf/hcf/classes/bard/BardData.java
+++ src/com/customhcf/hcf/classes/bard/BardData.java
@@ -1,22 +1,20 @@
+
 package com.customhcf.hcf.classes.bard;
 
 import com.google.common.base.Preconditions;
+import org.bukkit.scheduler.BukkitTask;
 
-public class BardData
-{
+public class BardData {
     public static final double MIN_ENERGY = 0.0;
     public static final double MAX_ENERGY = 100.0;
-    public static final long MAX_ENERGY_MILLIS = 100000L;
+    public static final long MAX_ENERGY_MILLIS = 100000;
     private static final double ENERGY_PER_MILLISECOND = 1.25;
     public long buffCooldown;
+    public BukkitTask heldTask;
     private long energyStart;
 
     public long getRemainingBuffDelay() {
         return this.buffCooldown - System.currentTimeMillis();
-    }
-
-    public long getRemainingBuffDelay(final long now) {
-        return this.buffCooldown - now;
     }
 
     public void startEnergyTracking() {
@@ -24,27 +22,21 @@ public class BardData
     }
 
     public long getEnergyMillis() {
-        if (this.energyStart == 0L) {
-            return 0L;
+        if (this.energyStart == 0) {
+            return 0;
         }
-        return Math.min(100000L, (long)(1.25 * (System.currentTimeMillis() - this.energyStart)));
-    }
-
-    public long getEnergyMillis(final long now) {
-        if (this.energyStart == 0L) {
-            return 0L;
-        }
-        return Math.min(100000L, (long)(1.25 * (now - this.energyStart)));
+        return Math.min(120000, (long)(1.25 * (double)(System.currentTimeMillis() - this.energyStart)));
     }
 
     public double getEnergy() {
-        final double value = this.getEnergyMillis() / 1000.0;
-        return Math.round(value * 10.0) / 10.0;
+        double value = (double)this.getEnergyMillis() / 1000.0;
+        return (double)Math.round(value * 10.0) / 10.0;
     }
 
-    public void setEnergy(final double energy) {
+    public void setEnergy(double energy) {
         Preconditions.checkArgument(energy >= 0.0, "Energy cannot be less than 0.0");
-        Preconditions.checkArgument(energy <= 100.0, "Energy cannot be more than 100.0");
-        this.energyStart = (long)(System.currentTimeMillis() - 1000.0 * energy);
+        Preconditions.checkArgument(energy <= 120.0, "Energy cannot be more than 120.0");
+        this.energyStart = (long)((double)System.currentTimeMillis() - 1000.0 * energy);
     }
 }
+
