e0d726420b5a874a6eeaba151accdf15033b8beb
unknown <sergivb01@gmail.com>
2017-10-11 20:21:39 +0200

    Added event list command

--- src/com/customhcf/hcf/kothgame/EventExecutor.java
+++ src/com/customhcf/hcf/kothgame/EventExecutor.java
@@ -2,23 +2,15 @@
 package com.customhcf.hcf.kothgame;
 
 import com.customhcf.hcf.HCF;
-import com.customhcf.hcf.kothgame.argument.EventCancelArgument;
-import com.customhcf.hcf.kothgame.argument.EventCreateArgument;
-import com.customhcf.hcf.kothgame.argument.EventDeleteArgument;
-import com.customhcf.hcf.kothgame.argument.EventRenameArgument;
-import com.customhcf.hcf.kothgame.argument.EventSetAreaArgument;
-import com.customhcf.hcf.kothgame.argument.EventSetCapzoneArgument;
-import com.customhcf.hcf.kothgame.argument.EventStartArgument;
-import com.customhcf.hcf.kothgame.argument.EventUptimeArgument;
-
+import com.customhcf.hcf.kothgame.argument.*;
 import com.customhcf.hcf.palace.EventSetCapzone;
 import com.customhcf.util.command.ArgumentExecutor;
-import com.customhcf.util.command.CommandArgument;
 
 public class EventExecutor
 extends ArgumentExecutor {
     public EventExecutor(HCF plugin) {
         super("event");
+        this.addArgument(new EventListArgument(plugin));
         this.addArgument(new EventCancelArgument(plugin));
         this.addArgument(new EventCreateArgument(plugin));
         this.addArgument(new EventDeleteArgument(plugin));
---
+++ src/com/customhcf/hcf/kothgame/argument/EventListArgument.java
@@ -0,0 +1,41 @@
+
+package com.customhcf.hcf.kothgame.argument;
+
+import com.customhcf.hcf.HCF;
+import com.customhcf.hcf.faction.type.Faction;
+import com.customhcf.hcf.kothgame.faction.EventFaction;
+import com.customhcf.hcf.kothgame.faction.KothFaction;
+import com.customhcf.util.command.CommandArgument;
+import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class EventListArgument extends CommandArgument {
+    private final HCF plugin;
+
+    public EventListArgument(HCF plugin) {
+        super("uptime", "Check the uptime of an event");
+        this.plugin = plugin;
+        this.permission = "hcf.command.event.argument." + this.getName();
+    }
+
+    public String getUsage(String label) {
+        return "" + '/' + label + ' ' + this.getName();
+    }
+
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        List<Faction> events = this.plugin.getFactionManager().getFactions().stream().filter(faction -> faction instanceof EventFaction).collect(Collectors.toList());
+
+        sender.sendMessage(ChatColor.GREEN + "Current events:");
+        for(Faction factionEvent : events){
+            sender.sendMessage(ChatColor.YELLOW + factionEvent.getName() + ChatColor.GRAY +
+                    " (" + ChatColor.GREEN + ((factionEvent instanceof KothFaction) ? "Koth" : "Conquest") + ChatColor.GRAY + ")");
+        }
+
+        return true;
+    }
+}
+
