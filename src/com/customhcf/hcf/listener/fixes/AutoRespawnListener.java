ddaf0bb5936d920225c2249970ede4c858b4aef2
unknown <sergivb01@gmail.com>
2017-10-13 22:59:35 +0200

    Added 1 tick delay to autorespawn so it works :/

--- src/com/customhcf/hcf/listener/fixes/AutoRespawnListener.java
+++ src/com/customhcf/hcf/listener/fixes/AutoRespawnListener.java
@@ -2,7 +2,9 @@ package com.customhcf.hcf.listener.fixes;
 
 import com.customhcf.hcf.HCF;
 import com.customhcf.hcf.utils.ConfigurationService;
+import me.sergivb01.giraffe.utils.TaskUtil;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.PlayerDeathEvent;
 
@@ -13,10 +15,10 @@ public class AutoRespawnListener implements Listener {
         this.plugin = plugin;
     }
 
-    @EventHandler
+    @EventHandler(ignoreCancelled = true, priority = EventPriority.MONITOR)
     public void onPlayerDie(PlayerDeathEvent event){
         if(ConfigurationService.KIT_MAP){
-            event.getEntity().getPlayer().spigot().respawn();
+            TaskUtil.runTaskNextTick(()-> event.getEntity().spigot().respawn());
         }
     }
 
