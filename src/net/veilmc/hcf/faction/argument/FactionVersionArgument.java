bbbcd697463ba54087736ec4c3676f8d6f7c786f
unknown <sergivb01@gmail.com>
2017-11-07 20:50:44 +0100

    Fixed merge issue

--- src/net/veilmc/hcf/faction/argument/FactionVersionArgument.java
+++ src/net/veilmc/hcf/faction/argument/FactionVersionArgument.java
@@ -21,21 +21,10 @@ public class FactionVersionArgument
     }
 
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-<<<<<<< HEAD
-        sender.sendMessage(ChatColor.GRAY + BukkitUtils.STRAIGHT_LINE_DEFAULT);
-        sender.sendMessage(ChatColor.GRAY + " ");
-        sender.sendMessage(ChatColor.WHITE + " * " + ChatColor.YELLOW.toString() + ChatColor.BOLD + HCF.getPlugin().getDescription().getName());
-        sender.sendMessage(ChatColor.WHITE + " * " + ChatColor.GRAY.toString() + "Version: " + ChatColor.GOLD + HCF.getPlugin().getDescription().getVersion());
-        sender.sendMessage(ChatColor.WHITE + " * " + ChatColor.GRAY.toString() + "Authors: " + ChatColor.GOLD + "Move2Linux & LTD");
-        sender.sendMessage(ChatColor.GRAY + " ");
-        sender.sendMessage(ChatColor.GRAY + BukkitUtils.STRAIGHT_LINE_DEFAULT);
-=======
-        Player p = (Player)sender;
-        p.sendMessage(ChatColor.translateAlternateColorCodes('&', "&8" + BukkitUtils.STRAIGHT_LINE_DEFAULT));
-        p.sendMessage(ChatColor.translateAlternateColorCodes('&', "&eThis server is running &6" + HCF.getPlugin().getDescription().getName() + "&e. Version: &6" + HCF.getPlugin().getDescription().getVersion()));
-        p.sendMessage(ChatColor.translateAlternateColorCodes('&', "&eCreated for use on &6Veil Network &7(veilmc.net/veilhcf.us)"));
-        p.sendMessage(ChatColor.translateAlternateColorCodes('&', "&8" + BukkitUtils.STRAIGHT_LINE_DEFAULT));
->>>>>>> 7f1c2655ebf03a56a444119ebb563b178094a16d
+        sender.sendMessage(ChatColor.translateAlternateColorCodes('&', "&8" + BukkitUtils.STRAIGHT_LINE_DEFAULT));
+        sender.sendMessage(ChatColor.translateAlternateColorCodes('&', "&eThis server is running &6" + HCF.getPlugin().getDescription().getName() + "&e. Version: &6" + HCF.getPlugin().getDescription().getVersion()));
+        sender.sendMessage(ChatColor.translateAlternateColorCodes('&', "&eCreated for use on &6Veil Network &7(veilmc.net/veilhcf.us)"));
+        sender.sendMessage(ChatColor.translateAlternateColorCodes('&', "&8" + BukkitUtils.STRAIGHT_LINE_DEFAULT));
         return true;
     }
 
