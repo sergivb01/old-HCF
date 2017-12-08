67f823ed950c24b2f820020b9da92944f06b26ed
explance <lentsalts@gmail.com>
2017-12-08 15:34:09 +0000

    removed redundant blacklist cmd, coords cmd message changed

--- src/net/veilmc/hcf/command/CoordsCommand.java
+++ src/net/veilmc/hcf/command/CoordsCommand.java
@@ -30,7 +30,7 @@ public class CoordsCommand
         List<Faction> events = this.plugin.getFactionManager().getFactions().stream().filter(faction -> faction instanceof EventFaction).collect(Collectors.toList());
         sender.sendMessage(ChatColor.translateAlternateColorCodes('&', "&7" + BukkitUtils.STRAIGHT_LINE_DEFAULT));
         sender.sendMessage(ChatColor.translateAlternateColorCodes('&', " "));
-        sender.sendMessage(ChatColor.translateAlternateColorCodes('&', " &6&lVeilMC Event Coordinates &7(Click)"));
+        sender.sendMessage(ChatColor.translateAlternateColorCodes('&', " &6&lEvent Coordinates &7(Click)"));
         sender.sendMessage(ChatColor.translateAlternateColorCodes('&', " "));
         int i = 1;
         for(Faction factionEvent : events){
