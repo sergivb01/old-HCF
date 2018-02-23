215376f0e008c186b67522eac6589aa072b02209
sergivb01 <sergivb01@gmail.com>
2018-02-23 20:59:28 +0100

    Fixed nullpointerexception on PotionLimiterListener

--- src/net/veilmc/hcf/listener/fixes/PotionLimitListener.java
+++ src/net/veilmc/hcf/listener/fixes/PotionLimitListener.java
@@ -19,59 +19,54 @@ import org.bukkit.projectiles.ProjectileSource;
 import java.util.List;
 
 public class PotionLimitListener
-  implements Listener
-{
-  private static HCF plugin;
-  private static List<Short> disabledPotions;
-  private static PotionLimiterData limiter;
-  
-  public boolean isPotionDisabled(ItemStack item)
-  {
-    return item.getType() == Material.POTION && disabledPotions.contains(Short.valueOf(item.getDurability()));
-  }
-  
-  public static void init() {}
-  
-  public static void reload()
-  {
-    disabledPotions = PotionLimiterData.getInstance().getConfig().getShortList("disabled-potions");
-    System.out.println(disabledPotions.toString());
-  }
-  
-  @EventHandler
-  public void onSplash(PotionSplashEvent event)
-  {
-    if (isPotionDisabled(event.getPotion().getItem()))
-    {
-      event.setCancelled(true);
-      ProjectileSource shooter = event.getEntity().getShooter();
-      if ((shooter instanceof Player)) {
-        ((Player)shooter).sendMessage(ChatColor.RED + "You cannot use this potion.");
-          ((Player) shooter).getPlayer().setItemInHand(null);
-      }
-    }
-  }
-  
-  @EventHandler
-  public void onPlayerItemConsume(PlayerItemConsumeEvent event)
-  {
-    if (isPotionDisabled(event.getItem()))
-    {
-      event.setCancelled(true);
-      event.getPlayer().sendMessage(ChatColor.RED + "You cannot use this potion.");
-      event.getPlayer().setItemInHand(null);
-    }
-  }
-  
-  @EventHandler
-  public void onBrew(BrewEvent e)
-  {
-    BrewerInventory inventory = e.getContents();
-    BrewingStand stand = inventory.getHolder();
-    stand.setBrewingTime(200);
-    if (isPotionDisabled(e.getContents().getItem(0))) {
-      e.setCancelled(true);
-    }
-  }
+		implements Listener{
+	private static HCF plugin;
+	private static List<Short> disabledPotions;
+	private static PotionLimiterData limiter;
+
+	public boolean isPotionDisabled(ItemStack item){
+		if(item == null || !disabledPotions.contains(item.getData().getItemTypeId())){
+			return false;
+		}
+		return item.getType() == Material.POTION && disabledPotions.contains(item.getTypeId());
+	}
+
+	public static void reload(){
+		disabledPotions = PotionLimiterData.getInstance().getConfig().getShortList("disabled-potions");
+		System.out.println(disabledPotions.toString());
+	}
+
+	@EventHandler
+	public void onSplash(PotionSplashEvent event){
+		if(isPotionDisabled(event.getPotion().getItem())){
+			event.setCancelled(true);
+			ProjectileSource shooter = event.getEntity().getShooter();
+			if((shooter instanceof Player)){
+				((Player) shooter).sendMessage(ChatColor.RED + "You cannot use this potion.");
+				((Player) shooter).getPlayer().setItemInHand(null);
+			}
+		}
+	}
+
+	@EventHandler
+	public void onPlayerItemConsume(PlayerItemConsumeEvent event){
+		if(isPotionDisabled(event.getItem())){
+			event.setCancelled(true);
+			event.getPlayer().sendMessage(ChatColor.RED + "You cannot use this potion.");
+			event.getPlayer().setItemInHand(null);
+		}
+	}
+
+	@EventHandler
+	public void onBrew(BrewEvent e){
+		BrewerInventory inventory = e.getContents();
+		BrewingStand stand = inventory.getHolder();
+		stand.setBrewingTime(200);
+		if(isPotionDisabled(e.getContents().getItem(0))){
+			e.setCancelled(true);
+		}
+	}
+
+
 }
 
