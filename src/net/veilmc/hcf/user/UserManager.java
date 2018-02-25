eb946c7cea60a32168f7e2806a7d19834ff9faf5
sergivb01 <sergivb01@gmail.com>
2018-02-25 22:33:18 +0100

    Added tab style to user profile

--- src/net/veilmc/hcf/user/UserManager.java
+++ src/net/veilmc/hcf/user/UserManager.java
@@ -13,62 +13,62 @@ import org.bukkit.event.player.PlayerJoinEvent;
 import java.util.*;
 
 public class UserManager
-implements Listener {
-    private final HCF plugin;
-    private final Map<UUID, FactionUser> users = new HashMap<>();
-    private Config userConfig;
+		implements Listener{
+	private final HCF plugin;
+	private final Map<UUID, FactionUser> users = new HashMap<>();
+	private Config userConfig;
 
-    public UserManager(HCF plugin) {
-        this.plugin = plugin;
-        this.reloadUserData();
-        plugin.getServer().getPluginManager().registerEvents(this, plugin);
-    }
+	public UserManager(HCF plugin){
+		this.plugin = plugin;
+		this.reloadUserData();
+		plugin.getServer().getPluginManager().registerEvents(this, plugin);
+	}
 
-    @EventHandler(ignoreCancelled=true, priority=EventPriority.MONITOR)
-    public void onPlayerJoin(PlayerJoinEvent event) {
-        UUID uuid = event.getPlayer().getUniqueId();
-        this.users.putIfAbsent(uuid, new FactionUser(uuid));
-    }
+	@EventHandler(ignoreCancelled = true, priority = EventPriority.MONITOR)
+	public void onPlayerJoin(PlayerJoinEvent event){
+		UUID uuid = event.getPlayer().getUniqueId();
+		this.users.putIfAbsent(uuid, new FactionUser(uuid));
+	}
 
-    public Map<UUID, FactionUser> getUsers() {
-        return this.users;
-    }
+	public Map<UUID, FactionUser> getUsers(){
+		return this.users;
+	}
 
-    
-    public FactionUser getUserAsync(UUID uuid) {
-        synchronized (this.users) {
-            FactionUser revert = new FactionUser(uuid);
-            FactionUser user = this.users.putIfAbsent(uuid, revert);
-            return ObjectUtils.firstNonNull(user, revert);
-        }
-    }
 
-    public FactionUser getUser(UUID uuid) {
-        FactionUser revert = new FactionUser(uuid);
-        FactionUser user = this.users.putIfAbsent(uuid, revert);
-        return ObjectUtils.firstNonNull(user, revert);
-    }
+	public FactionUser getUserAsync(UUID uuid){
+		synchronized(this.users){
+			FactionUser revert = new FactionUser(uuid);
+			FactionUser user = this.users.putIfAbsent(uuid, revert);
+			return ObjectUtils.firstNonNull(user, revert);
+		}
+	}
 
-    public void reloadUserData() {
-        this.userConfig = new Config(this.plugin, "faction-users");
-        final Object object = this.userConfig.get("users");
-        if (object instanceof MemorySection) {
-            final MemorySection section = (MemorySection)object;
-            final Collection<String> keys = section.getKeys(false);
-            for (final String id : keys) {
-                this.users.put(UUID.fromString(id), (FactionUser)this.userConfig.get(section.getCurrentPath() + '.' + id));
-            }
-        }
-    }
+	public FactionUser getUser(UUID uuid){
+		FactionUser revert = new FactionUser(uuid);
+		FactionUser user = this.users.putIfAbsent(uuid, revert);
+		return ObjectUtils.firstNonNull(user, revert);
+	}
 
-    public void saveUserData() {
-        Set<Map.Entry<UUID, FactionUser>> entrySet = this.users.entrySet();
-        LinkedHashMap<String, FactionUser> saveMap = new LinkedHashMap<String, FactionUser>(entrySet.size());
-        for (Map.Entry<UUID, FactionUser> entry : entrySet) {
-            saveMap.put(entry.getKey().toString(), entry.getValue());
-        }
-        this.userConfig.set("users", saveMap);
-        this.userConfig.save();
-    }
+	public void reloadUserData(){
+		this.userConfig = new Config(this.plugin, "faction-users");
+		final Object object = this.userConfig.get("users");
+		if(object instanceof MemorySection){
+			final MemorySection section = (MemorySection) object;
+			final Collection<String> keys = section.getKeys(false);
+			for(final String id : keys){
+				this.users.put(UUID.fromString(id), (FactionUser) this.userConfig.get(section.getCurrentPath() + '.' + id));
+			}
+		}
+	}
+
+	public void saveUserData(){
+		Set<Map.Entry<UUID, FactionUser>> entrySet = this.users.entrySet();
+		LinkedHashMap<String, FactionUser> saveMap = new LinkedHashMap<String, FactionUser>(entrySet.size());
+		for(Map.Entry<UUID, FactionUser> entry : entrySet){
+			saveMap.put(entry.getKey().toString(), entry.getValue());
+		}
+		this.userConfig.set("users", saveMap);
+		this.userConfig.save();
+	}
 }
 
