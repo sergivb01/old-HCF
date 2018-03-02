a679469f5bfd33ba355be955bf365f315ee1a381
explance <lentsalts@gmail.com>
2018-03-02 11:51:42 +0000

    EnchantSecurity bug fix

--- src/net/veilmc/hcf/listener/fixes/EnchantSecurityListener.java
+++ src/net/veilmc/hcf/listener/fixes/EnchantSecurityListener.java
@@ -4,6 +4,7 @@ import net.veilmc.util.BukkitUtils;
 import org.bukkit.Bukkit;
 import org.bukkit.enchantments.Enchantment;
 import org.bukkit.entity.Entity;
+import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
@@ -18,13 +19,15 @@ public class EnchantSecurityListener
         final Entity attackerEntity = event.getDamager();
         if (attackerEntity instanceof Player && BukkitUtils.getFinalAttacker(event, false) != null) {
             final Player attacker = (Player) attackerEntity;
-            final Player defender = (Player) event.getEntity();
             final ItemStack item = attacker.getItemInHand();
-            if ((item.getEnchantmentLevel(Enchantment.DAMAGE_ALL) > 5) || (item.getEnchantmentLevel(Enchantment.KNOCKBACK) > 5) || (item.getEnchantmentLevel(Enchantment.THORNS) > 5)) {
-                attacker.getInventory().removeItem(attacker.getInventory().getItemInHand());
-                Bukkit.getServer().dispatchCommand(Bukkit.getConsoleSender(), "ban " + attacker.getName() + " Illegal Enchants S:" + item.getEnchantmentLevel(Enchantment.DAMAGE_ALL) + " T:" + item.getEnchantmentLevel(Enchantment.THORNS) + " K:" + item.getEnchantmentLevel(Enchantment.KNOCKBACK) + " D:" + defender.getName());
-                event.setCancelled(true);
+            if(event.getEntityType().equals(EntityType.PLAYER)) {
+                final Player defender = (Player) event.getEntity();
+                if ((item.getEnchantmentLevel(Enchantment.DAMAGE_ALL) > 5) || (item.getEnchantmentLevel(Enchantment.KNOCKBACK) > 5) || (item.getEnchantmentLevel(Enchantment.THORNS) > 5)) {
+                    attacker.getInventory().removeItem(attacker.getInventory().getItemInHand());
+                    Bukkit.getServer().dispatchCommand(Bukkit.getConsoleSender(), "ban " + attacker.getName() + " Illegal Enchants S:" + item.getEnchantmentLevel(Enchantment.DAMAGE_ALL) + " T:" + item.getEnchantmentLevel(Enchantment.THORNS) + " K:" + item.getEnchantmentLevel(Enchantment.KNOCKBACK) + " D:" + defender.getName());
+                    event.setCancelled(true);
 
+                }
             }
         }
     }
