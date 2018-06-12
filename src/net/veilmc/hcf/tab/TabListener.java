c371a9e78a64c46cb11143e662b9d464223e93b2
sergivb01 <sergivb01@gmail.com>
2018-06-12 20:20:19 +0200

    test

--- src/net/veilmc/hcf/tab/TabListener.java
+++ src/net/veilmc/hcf/tab/TabListener.java
@@ -2,14 +2,17 @@ package net.veilmc.hcf.tab;
 
 import net.veilmc.hcf.HCF;
 import net.veilmc.hcf.tab.com.bizarrealex.azazel.Azazel;
+import org.bukkit.Bukkit;
 import org.bukkit.event.Listener;
 
 public class TabListener implements Listener{
 	private static HCF plugin;
 
 	public TabListener(HCF plugin){
-		new Azazel(plugin, new PlayerTab()); //Register azazel
+		new Azazel(plugin, new PlayerTab());
+		Bukkit.getPluginManager().registerEvents(this, plugin);
 	}
+
 
 
 }
